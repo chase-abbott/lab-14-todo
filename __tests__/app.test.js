7ed71b7bf6fd8e5205e7072efef3d409c46e3e67
AngelDOrtiz <aortizghs@gmail.com>
2021-05-13 15:37:28 -0700

    get api/me/todos TDD

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -46,39 +46,57 @@ describe('API Routes', () => {
       id: expect.any(Number),
       task: 'wash dishes',
       completed: false
-    }
+    };
 
-    let secondTodo = {
-      id: expect.any(Number),
-      task: 'clean floors',
-      completed: false
-    }
+    
     // append the token to your requests:
     //  .set('Authorization', user.token);
     it('POST /api/todos', async () => {
 
       const response = await request
-        .post(`/api/todos`)
+        .post('/api/todos')
         .set('Authorization', user.token)
         .send(todo);
       todo.userId = user.id;
-      secondTodo.userId = user2.id;
+      
 
       expect(response.status).toBe(200);
       expect(response.body).toEqual(todo);
-    })
+      todo = response.body;
+      console.log(todo);
+    });
 
 
-    it.skip('GET /api/me/todos', async () => {
+    it('GET /api/me/todos', async () => {
+      let secondTodo = {
+        id: expect.any(Number),
+        task: 'clean floors',
+        completed: false
+      };
+      
+      
+      const postResponse = await request
+        .post('/api/todos')
+        .set('Authorization', user2.token) 
+        .send(secondTodo);
+      expect(postResponse.status).toBe(200);
 
+      secondTodo = postResponse.body;
       // remove this line, here to not have lint error:
-      user.token;
+      
       const response = await request
-        .get('api/me/todos')
-        .set('Authorization', user.token)
+        .get('/api/me/todos')
+        .set('Authorization', user.token);
 
       expect(response.status).toBe(200);
-      // expect(response.body).toEqual(?);
+      expect(response.body).toEqual(expect.not.arrayContaining([secondTodo]));
+
+      const response2 = await request
+        .get('/api/me/todos')
+        .set('Authorization', user2.token);
+
+      expect(response2.status).toBe(200);
+      expect(response2.body).toEqual(expect.not.arrayContaining([todo]));
 
     });
 
