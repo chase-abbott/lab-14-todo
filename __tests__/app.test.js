39c98cc145a44db75622143907fc6b370177d048
Chase Abbott <cabbott93@gmail.com>
2021-05-13 14:45:28 -0700

    TDD post /api/todos

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,7 @@ import client from '../lib/client.js';
 import supertest from 'supertest';
 import app from '../lib/app.js';
 import { execSync } from 'child_process';
-import request from 'superagent';
+
 
 const request = supertest(app);
 
@@ -12,8 +12,9 @@ describe('API Routes', () => {
     return client.end();
   });
 
-  describe('/api/cats', () => {
+  describe('/api/todos', () => {
     let user;
+    let user2;
 
     beforeAll(async () => {
       execSync('npm run recreate-tables');
@@ -26,22 +27,59 @@ describe('API Routes', () => {
           password: 'password'
         });
 
+      const secondResponse = await request
+        .post('/api/auth/signup')
+        .send({
+          name: 'you the User',
+          email: 'you@user.com',
+          password: 'pw'
+        });
+
       expect(response.status).toBe(200);
+      expect(secondResponse.status).toBe(200);
 
       user = response.body;
+      user2 = secondResponse.body;
     });
 
+    let todo = {
+      id: expect.any(Number),
+      task: 'wash dishes',
+      completed: false
+    }
+
+    let secondTodo = {
+      id: expect.any(Number),
+      task: 'clean floors',
+      completed: false
+    }
     // append the token to your requests:
     //  .set('Authorization', user.token);
-    
-    it('VERB to /api/route [with context]', async () => {
-      
+    it('POST /api/todos', async () => {
+
+      const response = await request
+        .post(`/api/todos`)
+        .set('Authorization', user.token)
+        .send(todo);
+      todo.userId = user.id;
+      secondTodo.userId = user2.id;
+
+      expect(response.status).toBe(200);
+      expect(response.body).toEqual(todo);
+    })
+
+
+    it.skip('GET /api/me/todos', async () => {
+
       // remove this line, here to not have lint error:
       user.token;
-    
-      // expect(response.status).toBe(200);
+      const response = await request
+        .get('api/me/todos')
+        .set('Authorization', user.token)
+
+      expect(response.status).toBe(200);
       // expect(response.body).toEqual(?);
-      
+
     });
 
   });
